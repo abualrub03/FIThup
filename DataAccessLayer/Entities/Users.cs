5a91b2fe29cc4619213c7ef969a77ef2a3eba775
abualrub03 <89502136+abualrub03@users.noreply.github.com>
2024-04-01 00:49:02 +0300

    ss

--- DataAccessLayer/Entities/Users.cs
+++ DataAccessLayer/Entities/Users.cs
@@ -9,8 +9,16 @@ namespace Entities
     public class Users
     {
         public int UserID { get; set; }
-        public string Username { get; set; }
-        public string FullName { get; set; }
+        public string? Username { get; set; }
+        public string? FullName { get; set; }
+        public string? UniEmail { get; set; }
+        public string? Password { get; set; }
+        public string? LinkedInAccountLink{ get; set; }
+        public string? FacebookAccountLink { get; set; }
+        public string? InstagramAccountLink { get; set; }
+        public string? GitHupAccountLink { get; set; }
+        public string? ProfilePictureName { get; set; }
+        public string?  PhoneNumber { get; set; }
 
     }
 }
